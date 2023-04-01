d09ee28f18d44dbfd632dab3010d40cfab23d28c
vodovskoff <vodovskov.ea@students.dvfu.ru>
2023-04-01 12:47:20 +1000

    Добавить создание новых курсов

--- controllers/course.py
+++ controllers/course.py
@@ -1,8 +1,8 @@
 from app import app
-from flask import render_template
-from models.course import get_course, get_users_courses
+from flask import render_template, request
+from models.course import get_course, get_users_courses, Course
 from flask_login import current_user
-
+from app import db
 
 @app.route('/course/<id>')
 def course(id=None):
@@ -12,4 +12,14 @@ def course(id=None):
 def my_courses(id=None):
     course_info_list = get_users_courses(current_user.id)
     print(course_info_list)
-    return render_template('catalog.html', title_name="Ваши курсы", course_info_list=course_info_list, len=len, str=str)
+    return render_template('catalog.html', in_my_courses_list=True, title_name="Ваши курсы", course_info_list=course_info_list, len=len, str=str)
+
+@app.route('/new_course', methods=['POST'])
+def new_course(id=None):
+    course_info_list = get_users_courses(current_user.id)
+    course_name = request.form.get('course_name')
+    course_description = request.form.get('course_description')
+    course = Course(name=course_name, description=course_description, author_id=current_user.id)
+    db.session.add(course)
+    db.session.commit()
+    return render_template('catalog.html', in_my_courses_list=True, title_name="Ваши курсы", course_info_list=course_info_list, len=len, str=str)
